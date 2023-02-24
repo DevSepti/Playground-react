a352e8e3ef191a3e112968a99227512fa2dc3d6b
artem.korytov <artem.korytov@aitudala.kz>
2023-02-24 21:08:26 +0600

    feat: eslint added

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// eslint-disable import/no-extraneous-dependencies
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,4 +8,4 @@ export default defineConfig({
     port: 3000,
   },
   plugins: [react()],
-})
+});
